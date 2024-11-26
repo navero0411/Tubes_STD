fc53e697446a72bf0dc4c85da208914962c383f9
navero <navero.sap@gmail.com>
2024-11-26 20:48:49 +0700

    Initial Commit

---
+++ Tubes_STD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tubes_STD
+//
+//  Created by Navero Sandya on 26/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
