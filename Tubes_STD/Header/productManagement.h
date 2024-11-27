51e5c86ec27b50eb767283bf08ff6e44326c9eef
navero <navero.sap@gmail.com>
2024-11-27 17:44:39 +0700

    kode

---
+++ Tubes_STD/Header/productManagement.h
@@ -0,0 +1,12 @@
+//
+//  productManagement.h
+//  Tubes_STD
+//
+//  Created by Navero Sandya on 26/11/2024.
+//
+
+#ifndef productManagement_h
+#define productManagement_h
+
+
+#endif /* productManagement_h */
---
+++ Tubes_STD/Sources/productManagement.cpp
@@ -0,0 +1,8 @@
+//
+//  productManagement.cpp
+//  Tubes_STD
+//
+//  Created by Navero Sandya on 26/11/2024.
+//
+
+#include <stdio.h>
