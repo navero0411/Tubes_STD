fe7bdc3db06f64b625268df7123fc1f4638e30bd
navero <navero.sap@gmail.com>
2024-11-29 14:56:20 +0700

    nambah view

--- Tubes_STD/Sources/productManagement.cpp
+++ Tubes_STD/Sources/productManagement.cpp
@@ -54,7 +54,60 @@ void deleteAfter_Programmer(listProgrammer &LPGR, adrPgr PG, adrPgr prec);
 void deleteAfter_Project(listProject &LPRJ, adrPrj PJ, adrPrj prec );
 void deleteAfter_Penugasan(listProgrammer &LPGR, adrPgr PG, adrTugas PT, adrTugas prec);
 
-void view_Programmer(listProgrammer LPGR);
-void view_Project(listProject LPRJ);
-void view_Penugasan(listProgrammer LPGR);
-void view_All(listProgrammer LPGR, listProject LPRJ);
+void view_Programmer(listProgrammer LPGR) {
+    if (firstPGR(LPGR) == NULL) {
+        cout << "Maaf list kosong" << endl;
+    } else {
+        adrPgr p = firstPGR(LPGR);
+        cout << "Isi list programmer: ";
+        while (p != NULL) {
+            cout << infoPgr(p).username << ", " << infoPgr(p).totalProject << endl;
+            p = nextPgr(p);
+        }
+        cout << endl;
+    }
+}
+
+void view_Project(listProject LPRJ) {
+    if (firstPRJ(LPRJ) == NULL) {
+        cout << "Maaf list kosong" << endl;
+    } else {
+        adrPrj p = firstPRJ(LPRJ);
+        cout << "Isi list programmer: ";
+        while (p != NULL) {
+            cout << infoPrj(p).namaProject << ", " << infoPrj(p).banyakProgrammer << endl;
+            p = nextPrj(p);
+        }
+        cout << endl;
+    }
+}
+
+void view_Penugasan(listProgrammer LPGR, listProject LPRJ) {
+    if (firstPGR(LPGR) == NULL) {
+        cout << "Maaf tidak ada programmer" << endl;
+    } else {
+        adrPgr p = firstPGR(LPGR);
+        while (p != NULL) {
+            adrTugas q = firstPenugasan(p);
+            if (q == NULL) {
+                cout << "Maaf programmer ini tidak memiliki penugasan" << endl;
+            } else {
+                cout << "Tugas programmer " << infoPgr(p).username << ":" << endl;
+                while (q != NULL) {
+                    cout << project(q) << " | ";
+                    q = nextTugas(q);
+                }
+            }
+            p = nextPgr(p);
+        }
+        cout << endl;
+    }
+}
+
+void view_All(listProgrammer LPGR, listProject LPRJ) {
+    if (firstPGR(LPGR) == NULL || firstPRJ(LPRJ) == NULL) {
+        cout << "Maaf list tidak lengkap" << endl;
+    } else {
+        
+    }
+}
